7a194a1d0d9de52e13813ee44e24fb2c68aa6cf2
Vincent LAURENT <vince.laurent@gmail.com>
2020-12-28 17:42:45 +0100

    Titres des différents pages

--- apps/intranet/modules/activite/templates/rapportsSuccess.php
+++ apps/intranet/modules/activite/templates/rapportsSuccess.php
@@ -4,15 +4,17 @@
 
 	<?php include_partial('activite/filtersForm', array('parameters' => $parameters, 'saison' => $saison, 'commercialId' => $commercialId, 'comFiltered' => $comFiltered, 'produit' => $produit, 'from' => $from, 'to' => $to)); ?>
 
-	<div class="p-3 mb-2 bg-info text-white text-center rounded" style="margin-top: 24px; font-size: 1.25rem; padding: 0.5rem !important; "><strong><?php if ($client) echo $client; else echo $fournisseur; ?></strong></div>
-	<?php 
+	<div class="p-3 mb-2 bg-info text-white text-center rounded" style="margin-top: 24px; font-size: 1.25rem; padding: 0.5rem !important; "><strong><?php if ($client) echo $client; elseif($fournisseur) echo $fournisseur; elseif($commercial) echo $commercial; ?></strong></div>
+	<?php
+	$clientId = null;
+	$fournisseurId = null;
 	if ($client) {
 			$clientId = $client->getId();
-			$fournisseurId = null;
-		} else {
-			$clientId = null;
-			$fournisseurId = $fournisseur->getId();
 		}
+
+	if($fournisseur) {
+			$fournisseurId = $fournisseur->getId();
+	}
 	?>
 	<?php include_partial('activite/rapportPeriodique', array('from' => $from, 'to' => $to, 'detailsLink' => $detailsLink, 'activites' => $activitePeriode, 'devise' => $devise, 'clientId' => $clientId, 'fournisseurId' => $fournisseurId, 'activites1' => $activitePeriode1, 'activites2' => $activitePeriode2, 'parameters' => $parameters, 'titre' => 'Rapport périodique global')); ?>
 
@@ -21,20 +23,20 @@
 	<div class="p-3 text-dark text-center" style="margin-top: 24px">
 		<h4>---------- Détail par <?php if($client): ?>fournisseur<?php else: ?>client<?php endif; ?> ----------</h4>
 	</div>
-	
-	<?php 
-		foreach ($items as $item): 
-		
+
+	<?php
+		foreach ($items as $item):
+
 		if ($client) {
 			$clientId = $client->getId();
 			$fournisseurId = $item->getId();
 		} else {
 			$clientId = $item->getId();
-			$fournisseurId = $fournisseur->getId();
+			//$fournisseurId = $fournisseur->getId();
 		}
 	?>
 
-	<div class="p-3 mb-2 bg-info text-white text-center rounded" style="margin-top: 24px; font-size: 1.25rem; padding: 0.5rem !important; "><strong><?php if ($client): ?><?php echo $client ?><?php elseif($fournisseur): ?><?php echo $fournisseur ?><?php elseif($commercial): ?><?php echo $commercial; ?><?php endif; ?> / <?php echo $item ?></strong></div>
+	<div class="p-3 mb-2 bg-info text-white text-center rounded" style="margin-top: 24px; font-size: 1.25rem; padding: 0.5rem !important; "><strong><?php if ($client): ?><?php echo $client ?>&nbsp;/&nbsp;<?php elseif($fournisseur): ?><?php echo $fournisseur ?>&nbsp;/&nbsp;<?php endif; ?><?php echo $item ?> <?php if($commercial): ?><small>(<?php echo $commercial; ?>)</small><?php endif; ?></strong></div>
 
 	<?php include_partial('activite/rapportPeriodique', array('from' => $from, 'to' => $to, 'detailsLink' => $detailsLink, 'activites' => $activitePeriode, 'devise' => $devise, 'clientId' => $clientId, 'fournisseurId' => $fournisseurId, 'activites1' => $activitePeriode1, 'activites2' => $activitePeriode2, 'parameters' => $parameters, 'titre' => 'Rapport périodique')); ?>
 
