00c208455fd3bd28820a8880ef8c6c8af86df7c7
Jb Lm <lemetayer.jb@gmail.com>
2022-04-24 23:10:00 +0200

    colonnes du listing collection detail pour evol coupe

---
+++ apps/intranet/modules/collection/templates/_cd_date_commande.php
@@ -0,0 +1,2 @@
+<?php use_helper('Date'); ?>
+<?php echo format_date($collection_detail->getCollection()->getDateCommande(), 'dd/MM/yyyy'); ?>
---
+++ apps/intranet/modules/collection/templates/_cd_piece.php
@@ -0,0 +1,1 @@
+<?php echo $collection_detail->piece_categorie; ?> <?php echo $collection_detail->piece; ?>
