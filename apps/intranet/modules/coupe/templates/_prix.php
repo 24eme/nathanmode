ba694afe0d81cbc3a5b24477540bf5d72cb75679
Vincent LAURENT <vince.laurent@gmail.com>
2022-06-07 17:23:15 +0200

    input float dans la liste

--- apps/intranet/modules/coupe/templates/_prix.php
+++ apps/intranet/modules/coupe/templates/_prix.php
@@ -1,4 +1,4 @@
-<input class="submit_ajax_on_change input-discreet" form="form_coupe_<?php echo $coupe->getId() ?>" name="coupe_ligne[prix]" data-partialview="<?php echo url_for('coupe_ligne_view', array('id' => $coupe->getId(), 'partial' => 'prix')) ?>" style="opacity: 1; <?php if($coupe->getPrix()): ?>display: none;<?php endif; ?>" type="text" value="<?php echo $coupe->getPrix(); ?>" />
+<input class="submit_ajax_on_change input-discreet input-float" form="form_coupe_<?php echo $coupe->getId() ?>" name="coupe_ligne[prix]" data-partialview="<?php echo url_for('coupe_ligne_view', array('id' => $coupe->getId(), 'partial' => 'prix')) ?>" style="opacity: 1; <?php if($coupe->getPrix()): ?>display: none;<?php endif; ?>" type="text" value="<?php echo $coupe->getPrix(); ?>" />
 <?php if($coupe->getPrix()): ?>
 <span class="clic2showfield"><?php echo $coupe->getPrix(); ?></span>
 <?php endif; ?>
