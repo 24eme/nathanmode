4ab689299620e6fae3be9a3891aff324e874cd04
Jb Lm <lemetayer.jb@gmail.com>
2022-06-07 15:00:36 +0200

    lien de retour a la suppression d'une production soldée

--- apps/intranet/modules/production/actions/actions.class.php
+++ apps/intranet/modules/production/actions/actions.class.php
@@ -120,4 +120,20 @@ protected function buildQuerySoldees()
 
     $this->setTemplate('index');
   }
+
+  public function executeDelete(sfWebRequest $request)
+  {
+    $request->checkCSRFProtection();
+
+    $this->dispatcher->notify(new sfEvent($this, 'admin.delete_object', array('object' => $this->getRoute()->getObject())));
+    $situation = $this->getRoute()->getObject()->getSituation();
+    /*if ($this->getRoute()->getObject()->delete())
+    {
+      $this->getUser()->setFlash('notice', 'The item was deleted successfully.');
+    }*/
+    if ($situation === Situations::SITUATION_SOLDEE)
+      $this->redirect('collection_production_collection', array('action' => 'CommandesSoldees'));
+    else
+      $this->redirect('@collection_production');
+  }
 }
