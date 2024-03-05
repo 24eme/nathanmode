0e846f2fc95c158b82df6f638d8f76b8c2dbfe6c
Jb Lm <lemetayer.jb@gmail.com>
2024-03-05 15:15:34 +0100

    widget datepicker

---
+++ lib/widget/WidgetFormInputDate.class.php
@@ -0,0 +1,18 @@
+<?php
+class WidgetFormInputDate extends sfWidgetFormInput
+{
+  /**
+   * Configures the current widget.
+   *
+   * @param array $options     An array of options
+   * @param array $attributes  An array of default HTML attributes
+   *
+   * @see sfWidgetForm
+   */
+  protected function configure($options = array(), $attributes = array())
+  {
+    parent::configure($options, $attributes);
+
+    $this->setOption('type', 'date');
+  }
+}
