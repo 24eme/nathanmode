d97284fe6ba6250f7cd8aff9121765d842318fb1
Jb Lm <lemetayer.jb@gmail.com>
2024-03-08 11:04:07 +0100

    refacto

--- lib/model/Activite.class.php
+++ lib/model/Activite.class.php
@@ -18,144 +18,50 @@ class Activite
 		"PLAIDS",
 	);
 
-	public function __construct($from, $to, $saison, $commercial, $produit)
-	{
+	public function __construct($from, $to, $saison, $commercial, $produit) {
 		$this->from = $from;
 		$this->to = $to;
 		$this->saison = $saison;
 		$this->commercial = $commercial;
 		$this->produit = $produit;
 	}
-	
-	public function getMontant($devise = 1, $client = null, $fournisseur = null)
-	{
-		$where = "";
-		if ($client) {
-			$where .= " AND b.client_id = ".$client;
-		}
-		if ($fournisseur) {
-			$where .= " AND b.fournisseur_id = ".$fournisseur;
-		}
-		if ($this->saison) {
-			$where .= " AND b.saison_id = ".$this->saison;
-		}
-		if ($this->commercial) {
-			$where .= " AND b.commercial_id = ".$this->commercial;
-		}
-		if ($this->produit == 'mts') {
-			$where .= " AND b.metrage IS NOT NULL AND b.metrage != ''";
-		}
-		if ($this->produit == 'pcs') {
-			$where .= " AND b.piece IS NOT NULL AND b.piece != ''";
-		}
-		if (preg_match("/^pcs_/", $this->produit)) {
-			$where .= " AND b.piece_categorie = '".str_replace("pcs_", "", $this->produit)."' AND b.piece IS NOT NULL AND b.piece != ''";
-		}
 
-		$reqFacture = "SELECT SUM(b.montant) as montant FROM commande b WHERE b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise." AND b.montant > 0".$where;
-		$reqCredit = "SELECT SUM(b.montant_total) as montant FROM bon b WHERE b.type != 'Facture' AND b.statut IN ('DEDUITE','EN_ATTENTE','PAYEE') AND b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise.$where;
-		$req = "SELECT (".$reqFacture.") as facture, (".$reqCredit.") as credit, (SELECT ifnull(facture,0) - ifnull(credit,0)) as total";
-
-		$result = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAssoc($req);
-
-		return ($result[0]['total'])? $result[0]['total'] : 0;
+	public function getMontant($devise = 1, $client = null, $fournisseur = null) {
+		$where = $this->getConditions($client, $fournisseur);
+		return $this->getTotalCalcule('montant', $devise, $where);
 
 	}
 
-	public function getCom($devise = 1, $client = null, $fournisseur = null)
-	{
-		$where = "";
-		if ($client) {
-			$where .= " AND b.client_id = ".$client;
-		}
-		if ($fournisseur) {
-			$where .= " AND b.fournisseur_id = ".$fournisseur;
-		}
-		if ($this->saison) {
-			$where .= " AND b.saison_id = ".$this->saison;
-		}
-		if ($this->commercial) {
-			$where .= " AND b.commercial_id = ".$this->commercial;
-		}
-		if ($this->produit == 'mts') {
-			$where .= " AND b.metrage IS NOT NULL AND b.metrage != ''";
-		}
-		if ($this->produit == 'pcs') {
-			$where .= " AND b.piece IS NOT NULL AND b.piece != ''";
-		}
-		if (preg_match("/^pcs_/", $this->produit)) {
-			$where .= " AND b.piece_categorie = '".str_replace("pcs_", "", $this->produit)."' AND b.piece IS NOT NULL AND b.piece != ''";
-		}
-
-		$reqFacture = "SELECT SUM(b.total_fournisseur) as montant FROM commande b WHERE b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise." AND b.montant > 0".$where;
-		$reqCredit = "SELECT SUM(b.total_fournisseur) as montant FROM bon b WHERE b.type != 'Facture' AND b.statut IN ('DEDUITE','EN_ATTENTE','PAYEE') AND b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise.$where;
-		$req = "SELECT (".$reqFacture.") as facture, (".$reqCredit.") as credit, (SELECT ifnull(facture,0) - ifnull(credit,0)) as total";
-
-		$result = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAssoc($req);
-
-		return ($result[0]['total'])? $result[0]['total'] : 0;
-
+	public function getCom($devise = 1, $client = null, $fournisseur = null) {
+		$where = $this->getConditions($client, $fournisseur);
+		return $this->getTotalCalcule('total_fournisseur', $devise, $where);
 	}
 
-	public function getMts($devise = 1, $client = null, $fournisseur = null)
-	{
-		$where = "";
-		if ($client) {
-			$where .= " AND b.client_id = ".$client;
-		}
-		if ($fournisseur) {
-			$where .= " AND b.fournisseur_id = ".$fournisseur;
-		}
-		if ($this->saison) {
-			$where .= " AND b.saison_id = ".$this->saison;
-		}
-		if ($this->commercial) {
-			$where .= " AND b.commercial_id = ".$this->commercial;
-		}
-		if ($this->produit == 'mts') {
-			$where .= " AND b.metrage IS NOT NULL AND b.metrage != ''";
-		}
-		if ($this->produit == 'pcs') {
-			$where .= " AND b.piece IS NOT NULL AND b.piece != ''";
-		}
-		if (preg_match("/^pcs_/", $this->produit)) {
-			$where .= " AND b.piece_categorie = '".str_replace("pcs_", "", $this->produit)."' AND b.piece IS NOT NULL AND b.piece != ''";
-		}
-
-		$reqFacture = "SELECT SUM(b.metrage) as montant FROM commande b WHERE b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise." AND b.montant > 0".$where;
-		$reqCredit = "SELECT SUM(b.metrage) as montant FROM bon b WHERE b.type != 'Facture' AND b.statut IN ('DEDUITE','EN_ATTENTE','PAYEE') AND b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise.$where;
-		$req = "SELECT (".$reqFacture.") as facture, (".$reqCredit.") as credit, (SELECT ifnull(facture,0) - ifnull(credit,0)) as total";
-
-		$result = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAssoc($req);
-
-		return ($result[0]['total'])? $result[0]['total'] : 0;
-
+	public function getMts($devise = 1, $client = null, $fournisseur = null) {
+		$where = $this->getConditions($client, $fournisseur);
+		return $this->getTotalCalcule('metrage', $devise, $where);
 	}
 
-	public function getDetailledPcs($devise = 1, $client = null, $fournisseur = null)
-	{
-		$where = "";
-		if ($client) {
-			$where .= " AND b.client_id = ".$client;
+	public function getPcsAccessoires($devise = 1, $client = null, $fournisseur = null) {
+		return $this->getPcs($devise, $client, $fournisseur, true);
+	}
+
+	public function getPcsNonAccessoires($devise = 1, $client = null, $fournisseur = null) {
+		return $this->getPcs($devise, $client, $fournisseur, false);
+	}
+
+	private function getPcs($devise = 1, $client = null, $fournisseur = null, $accessoire = true) {
+		$where = $this->getConditions($client, $fournisseur);
+		if ($accessoire) {
+			$where .= " AND b.piece_categorie IN ('".implode("','", self::$ACCESSOIRES_CATEGORIES)."')";
+		} else {
+			$where .= " AND (b.piece_categorie NOT IN ('".implode("','", self::$ACCESSOIRES_CATEGORIES)."') OR b.piece_categorie IS NULL OR b.piece_categorie = '')";
 		}
-		if ($fournisseur) {
-			$where .= " AND b.fournisseur_id = ".$fournisseur;
-		}
-		if ($this->saison) {
-			$where .= " AND b.saison_id = ".$this->saison;
-		}
-		if ($this->commercial) {
-			$where .= " AND b.commercial_id = ".$this->commercial;
-		}
-		if ($this->produit == 'mts') {
-			$where .= " AND b.metrage IS NOT NULL AND b.metrage != ''";
-		}
-		if ($this->produit == 'pcs') {
-			$where .= " AND b.piece IS NOT NULL AND b.piece != ''";
-		}
-		if (preg_match("/^pcs_/", $this->produit)) {
-			$where .= " AND b.piece_categorie = '".str_replace("pcs_", "", $this->produit)."' AND b.piece IS NOT NULL AND b.piece != ''";
-		}
+		return $this->getTotalCalcule('piece', $devise, $where);
+	}
+
+	public function getDetailledPcs($devise = 1, $client = null, $fournisseur = null) {
+		$where = $this->getConditions($client, $fournisseur);
 
 		$reqCredit = "SELECT b.piece_categorie as categorie, SUM(b.piece) as montant FROM bon b WHERE b.type != 'Facture' AND b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise." AND b.montant > 0".$where." GROUP BY categorie";
 		$items = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAssoc($reqCredit);
@@ -180,93 +86,17 @@ class Activite
 		return $resultFacture;
 	}
 
-	private function getPcs($devise = 1, $client = null, $fournisseur = null, $accessoire = true)
-	{
-		$where = "";
-		if ($client) {
-			$where .= " AND b.client_id = ".$client;
-		}
-		if ($fournisseur) {
-			$where .= " AND b.fournisseur_id = ".$fournisseur;
-		}
-		if ($this->saison) {
-			$where .= " AND b.saison_id = ".$this->saison;
-		}
-		if ($this->commercial) {
-			$where .= " AND b.commercial_id = ".$this->commercial;
-		}
-		if ($this->produit == 'mts') {
-			$where .= " AND b.metrage IS NOT NULL AND b.metrage != ''";
-		}
-		if ($this->produit == 'pcs') {
-			$where .= " AND b.piece IS NOT NULL AND b.piece != ''";
-		}
-		if ($accessoire) {
-			$where .= " AND b.piece_categorie IN ('".implode("','", self::$ACCESSOIRES_CATEGORIES)."')";
-		} else {
-			$where .= " AND (b.piece_categorie NOT IN ('".implode("','", self::$ACCESSOIRES_CATEGORIES)."') OR b.piece_categorie IS NULL OR b.piece_categorie = '')";
-		}
-		if (preg_match("/^pcs_/", $this->produit)) {
-			$where .= " AND b.piece_categorie = '".str_replace("pcs_", "", $this->produit)."' AND b.piece IS NOT NULL AND b.piece != ''";
-		}
-
-		$reqFacture = "SELECT SUM(b.piece) as montant FROM commande b WHERE b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise." AND b.montant > 0".$where;
-		$reqCredit = "SELECT SUM(b.piece) as montant FROM bon b WHERE b.type != 'Facture' AND b.statut IN ('DEDUITE','EN_ATTENTE','PAYEE') AND b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise.$where;
-
-		$req = "SELECT (".$reqFacture.") as facture, (".$reqCredit.") as credit, (SELECT ifnull(facture,0) - ifnull(credit,0)) as total";
-
-		$result = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAssoc($req);
-
-		return ($result[0]['total'])? $result[0]['total'] : 0;
-
-	}
-
-	public function getPcsAccessoires($devise = 1, $client = null, $fournisseur = null)
-	{
-		return $this->getPcs($devise, $client, $fournisseur, true);
-	}
-
-	public function getPcsNonAccessoires($devise = 1, $client = null, $fournisseur = null)
-	{
-		return $this->getPcs($devise, $client, $fournisseur, false);
-	}
-
-	public function getDetails($devise = 1, $client = null, $fournisseur = null)
-	{
-		$where = "";
-		if ($client) {
-			$where .= " AND b.client_id = ".$client;
-		}
-		if ($fournisseur) {
-			$where .= " AND b.fournisseur_id = ".$fournisseur;
-		}
-		if ($this->saison) {
-			$where .= " AND b.saison_id = ".$this->saison;
-		}
-		if ($this->commercial) {
-			$where .= " AND b.commercial_id = ".$this->commercial;
-		}
-		if ($this->produit == 'mts') {
-			$where .= " AND b.metrage IS NOT NULL AND b.metrage != ''";
-		}
-		if ($this->produit == 'pcs') {
-			$where .= " AND b.piece IS NOT NULL AND b.piece != ''";
-		}
-		if (preg_match("/^pcs_/", $this->produit)) {
-			$where .= " AND b.piece_categorie = '".str_replace("pcs_", "", $this->produit)."' AND b.piece IS NOT NULL AND b.piece != ''";
-		}
-
+	public function getDetails($devise = 1, $client = null, $fournisseur = null) {
+		$where = $this->getConditions($client, $fournisseur);
 		$reqFacture = "SELECT 1 as coef, s.libelle, b.qualite, sum(b.metrage) as metrage, sum(b.piece) as piece, sum(b.montant) as montant FROM commande b INNER JOIN saison s ON b.saison_id = s.id WHERE b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise." AND b.montant > 0".$where." GROUP BY s.libelle, b.qualite";
 		$reqCredit = "SELECT -1 as coef, s.libelle, b.qualite, sum(b.metrage) as metrage, sum(b.piece) as piece, sum(b.montant_total) as montant FROM bon b INNER JOIN saison s ON b.saison_id = s.id WHERE b.type != 'Facture' AND b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise." AND b.montant_total > 0".$where." GROUP BY s.libelle, b.qualite";
 
 		$result = array_merge(Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAssoc($reqFacture), Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAssoc($reqCredit));
 
 		return $result;
-
 	}
 
-	public function getMontantByClient()
-	{
+	public function getMontantByClient() {
 		$reqGlobal = "SELECT c.id as id, c.raison_sociale as client FROM client c ORDER BY client ASC";
 		$reqFacture = "SELECT c.id as id, c.raison_sociale as client, SUM(b.montant) as montant FROM commande b JOIN client c ON b.client_id = c.id WHERE b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = 1 AND b.montant > 0 GROUP BY b.client_id";
 		$reqCredit = "SELECT c.id as id, c.raison_sociale as client, SUM(b.montant_total) as montant FROM bon b JOIN client c ON b.client_id = c.id WHERE b.type != 'Facture' AND b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = 1 AND b.montant_total > 0 GROUP BY b.client_id";
@@ -285,11 +115,9 @@ class Activite
 			$factures[$rc['id']]['total'] = $factures[$rc['id']]['total'] - $rc['montant'];
 		}
 		return $factures;
-
 	}
 
-	public function getMontantByGlobal()
-	{
+	public function getMontantByGlobal() {
 		$reqGlobal = "SELECT CONCAT(b.saison_id, b.client_id, b.fournisseur_id, b.qualite, b.metrage) as id, s.libelle as saison, c.raison_sociale as client, f.raison_sociale as fournisseur, b.qualite as qualite, b.metrage as metrage FROM commande b JOIN client c ON b.client_id = c.id JOIN saison s ON b.saison_id = s.id JOIN fournisseur f ON b.fournisseur_id = f.id";
 		$reqFacture = "SELECT CONCAT(b.saison_id, b.client_id, b.fournisseur_id, b.qualite, b.metrage) as id, s.libelle as saison, c.raison_sociale as client, f.raison_sociale as fournisseur, b.qualite as qualite, b.metrage as metrage, SUM(b.montant) as montant FROM commande b JOIN client c ON b.client_id = c.id JOIN saison s ON b.saison_id = s.id JOIN fournisseur f ON b.fournisseur_id = f.id WHERE b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = 1 AND b.montant > 0 GROUP BY id";
 		$reqCredit = "SELECT CONCAT(b.saison_id, b.client_id, b.fournisseur_id, b.qualite, b.metrage) as id, s.libelle as saison, c.raison_sociale as client, f.raison_sociale as fournisseur, b.qualite as qualite, b.metrage as metrage, SUM(b.montant_total) as montant FROM bon b JOIN client c ON b.client_id = c.id JOIN saison s ON b.saison_id = s.id JOIN fournisseur f ON b.fournisseur_id = f.id WHERE b.type != 'Facture' AND b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = 1 AND b.montant_total > 0 GROUP BY id";
@@ -308,6 +136,40 @@ class Activite
 			$factures[$rc['id']]['total'] = $factures[$rc['id']]['total'] - $rc['montant'];
 		}
 		return $factures;
-		
+	}
+
+	protected function getConditions($client = null, $fournisseur = null) {
+		$where = "";
+		if ($client) {
+			$where .= " AND b.client_id = ".$client;
+		}
+		if ($fournisseur) {
+			$where .= " AND b.fournisseur_id = ".$fournisseur;
+		}
+		if ($this->saison) {
+			$where .= " AND b.saison_id = ".$this->saison;
+		}
+		if ($this->commercial) {
+			$where .= " AND b.commercial_id = ".$this->commercial;
+		}
+		if ($this->produit == 'mts') {
+			$where .= " AND b.metrage IS NOT NULL AND b.metrage != ''";
+		}
+		if ($this->produit == 'pcs') {
+			$where .= " AND b.piece IS NOT NULL AND b.piece != ''";
+		}
+		if (preg_match("/^pcs_/", $this->produit)) {
+			$where .= " AND b.piece_categorie = '".str_replace("pcs_", "", $this->produit)."' AND b.piece IS NOT NULL AND b.piece != ''";
+		}
+		return $where;
+	}
+
+	protected function getTotalCalcule($field, $devise, $where) {
+		$reqFacture = "SELECT SUM(b.$field) as montant FROM commande b WHERE b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise." AND b.montant > 0".$where;
+		$reqCredit = "SELECT SUM(b.$field) as montant FROM bon b WHERE b.type != 'Facture' AND b.statut IN ('DEDUITE','EN_ATTENTE','PAYEE') AND b.date <= '".$this->to."' AND b.date >= '".$this->from."' AND b.devise_montant_id = ".$devise.$where;
+		$reqTotal = "SELECT (".$reqFacture.") as facture, (".$reqCredit.") as credit, (SELECT ifnull(facture,0) - ifnull(credit,0)) as total";
+		$result = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAssoc($reqTotal);
+
+		return ($result[0]['total'])? $result[0]['total'] : 0;
 	}
 }
