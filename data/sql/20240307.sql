bed029f09a188fa1ce28aa7c53d32fb94d27051d
Jb Lm <lemetayer.jb@gmail.com>
2024-03-15 15:56:21 +0100

    nouvelle table pour logger les indicateurs du commercial activity

--- data/sql/20240307.sql
+++ data/sql/20240307.sql
@@ -1,1 +1,4 @@
 ALTER TABLE `coupe` ADD `fichier_confirmation` VARCHAR(255) NULL AFTER `nb_relance`;
+
+
+CREATE TABLE commercial_activity_logger(id BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT, date_log DATETIME NOT NULL, ca DOUBLE(18,2), com DOUBLE(18,2), mts DOUBLE(18,2), acces DOUBLE(18,2), pf_cn DOUBLE(18,2));
