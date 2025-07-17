99d1c4a7b0152e8096fea64f534a472d4aac0158
JB LM <lemetayer.jb@gmail.com>
2025-07-17 18:43:33 +0200

    organisation du formulaire collection infos generales en 3 colonnes avec les champs générique devise et piece categorie

--- apps/intranet/modules/collection/templates/_formInfosGenerales.php
+++ apps/intranet/modules/collection/templates/_formInfosGenerales.php
@@ -2,10 +2,10 @@
     <div class="titre"><span>Infos générales</span></div>
     <div id="alertBox" class="bg-danger" style="float:left;width:100%; margin-top: -10px;"></div>
     <div class="px-2">
-        <table width="50%" border="0" cellpadding="0" cellspacing="0" class="tabloInfoGen">
+        <table width="33%" border="0" cellpadding="0" cellspacing="0" class="tabloInfoGen">
             <tr>
                 <td width="150"><?php echo $form['saison_id']->renderLabel() ?>&nbsp;:</td>
-                <td colpsan="2">
+                <td>
                     <?php echo $form['saison_id']->render(['required' => 'required']) ?>
                     <?php echo $form['saison_id']->renderError() ?>
                 </td>
@@ -16,11 +16,6 @@
                     <?php echo $form['fournisseur_id']->render(['required' => 'required']) ?>
                     <?php echo $form['fournisseur_id']->renderError() ?>
                 </td>
-                <td>
-                  <?php echo $form['prix_fournisseur']->render(array('class' => 'small input-float', 'required' => 'required')) ?>
-                  <?php echo $form['devise_fournisseur_id']->render(array('class' => 'small')) ?>
-                  <?php echo $form['prix_fournisseur']->renderError() ?>
-                </td>
             </tr>
             <tr>
                 <td><?php echo $form['commercial_id']->renderLabel() ?>&nbsp;:</td>
@@ -28,33 +23,58 @@
                     <?php echo $form['commercial_id']->render() ?>
                     <?php echo $form['commercial_id']->renderError() ?>
                 </td>
-                <td>
-                    <?php echo $form['prix_commercial']->render(array('class' => 'small input-float')) ?>
-                    <?php echo $form['devise_commercial_id']->render(array('class' => 'small')) ?>
-                    <?php echo $form['prix_commercial']->renderError() ?>
-                </td>
             </tr>
             <tr>
                 <td><?php echo $form['client_id']->renderLabel() ?>&nbsp;:</td>
-                <td colpsan="2">
+                <td>
                     <?php echo $form['client_id']->render(['required' => 'required']) ?>
                     <?php echo $form['client_id']->renderError() ?>
                 </td>
             </tr>
             <tr>
-                <td><?php echo $form['paiement']->renderLabel() ?>&nbsp;:</td>
-                <td colpsan="2">
-                    <?php echo $form['paiement']->render(['required' => 'required']) ?>
-                    <?php echo $form['paiement']->renderError() ?>
-                </td>
+              <td><?php echo $form['paiement']->renderLabel() ?>&nbsp;:</td>
+              <td>
+                  <?php echo $form['paiement']->render(['required' => 'required']) ?>
+                  <?php echo $form['paiement']->renderError() ?>
+              </td>
             </tr>
-        </table>
-        <table width="50%" border="0" cellpadding="0" cellspacing="0">
             <tr>
-                <td width="150"><?php echo $form['situation']->renderLabel() ?>&nbsp;:</td>
+                <td><?php echo $form['situation']->renderLabel() ?>&nbsp;:</td>
                 <td>
                     <?php echo $form['situation']->render() ?>
                     <?php echo $form['situation']->renderError() ?>
+                </td>
+            </tr>
+        </table>
+        <table width="33%" border="0" cellpadding="0" cellspacing="0">
+          <tr>
+              <td width="150"><?php echo $form['num_commande']->renderLabel() ?>&nbsp;:</td>
+              <td colpsan="2">
+                  <?php echo $form['num_commande']->render(['required' => 'required']) ?>
+                  <?php echo $form['num_commande']->renderError() ?>
+              </td>
+          </tr>
+          <tr>
+              <td><?php echo $form['date_commande']->renderLabel() ?>&nbsp;:</td>
+              <td style="text-align:left;" colpsan="2">
+                  <?php echo $form['date_commande']->render(['required' => 'required']) ?>
+                  <?php echo $form['date_commande']->renderError() ?>
+              </td>
+          </tr>
+          <tr>
+              <td style="vertical-align:top;">
+                  <?php echo $form['fichier']->renderLabel() ?><img src="/css/img/pdf.gif" width="18" height="19" alt="" align="absbottom" />&nbsp;:
+              </td>
+              <td class="uploadFile" colpsan="2">
+                  <?php echo $form['fichier']->render(array('class' => 'input')) ?>
+                  <?php echo $form['fichier']->renderError() ?>
+              </td>
+          </tr>
+            <tr>
+                <td><?php echo $form['piece_categorie']->renderLabel() ?>&nbsp;:</td>
+                <td>
+                    <?php echo $form['piece_categorie']->render(array('class' => 'chosen')) ?>
+                    <?php echo $form['piece_categorie']->renderError() ?>
                 </td>
             </tr>
             <tr>
@@ -76,30 +96,34 @@
                     <?php echo $form['ecru']->renderError() ?>
                 </td>
               </tr>
-              <tr>
-                  <td><?php echo $form['num_commande']->renderLabel() ?>&nbsp;:</td>
-                  <td colpsan="2">
-                      <?php echo $form['num_commande']->render(['required' => 'required']) ?>
-                      <?php echo $form['num_commande']->renderError() ?>
-                  </td>
-              </tr>
-              <tr>
-                  <td><?php echo $form['date_commande']->renderLabel() ?>&nbsp;:</td>
-                  <td style="text-align:left;" colpsan="2">
-                      <?php echo $form['date_commande']->render(['required' => 'required']) ?>
-                      <?php echo $form['date_commande']->renderError() ?>
-                  </td>
-              </tr>
-              <tr>
-                  <td style="vertical-align:top;">
-                      <?php echo $form['fichier']->renderLabel() ?><img src="/css/img/pdf.gif" width="18" height="19" alt="" align="absbottom" />&nbsp;:
-                  </td>
-                  <td class="uploadFile" colpsan="2">
-                      <?php echo $form['fichier']->render(array('class' => 'input')) ?>
-                      <?php echo $form['fichier']->renderError() ?>
+          </table>
+
+          <table width="33%" border="0" cellpadding="0" cellspacing="0">
+            <tr>
+                <td  width="150"><?php echo $form['devise_id']->renderLabel() ?>&nbsp;:</td>
+                <td>
+                    <?php echo $form['devise_id']->render(array('class' => 'chosen')) ?>
+                    <?php echo $form['devise_id']->renderError() ?>
+                </td>
+            </tr>
+            <tr>
+                <td><?php echo $form['prix_fournisseur']->renderLabel() ?>&nbsp;:</td>
+                <td>
+                  <?php echo $form['prix_fournisseur']->render(array('class' => 'small input-float', 'required' => 'required')) ?>
+                  <?php echo $form['devise_fournisseur_id']->render(array('class' => 'small')) ?>
+                  <?php echo $form['prix_fournisseur']->renderError() ?>
+                </td>
+            </tr>
+            <tr>
+              <td><?php echo $form['prix_commercial']->renderLabel() ?>&nbsp;:</td>
+              <td>
+                  <?php echo $form['prix_commercial']->render(array('class' => 'small input-float')) ?>
+                  <?php echo $form['devise_commercial_id']->render(array('class' => 'small')) ?>
+                  <?php echo $form['prix_commercial']->renderError() ?>
                   </td>
               </tr>
           </table>
+
           <table width="100%" border="0" cellpadding="0" cellspacing="0">
             <tr>
               <td width="150"><?php echo $form['observation_general']->renderLabel() ?> :</td>
