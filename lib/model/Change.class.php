eec017e02d561f7fccafd9fefccec2ae5db9aad2
JB LM <lemetayer.jb@gmail.com>
2025-07-22 18:32:00 +0200

    class de récupération des taux de change eur / usd via l'api libre frankfurter

---
+++ lib/model/Change.class.php
@@ -0,0 +1,76 @@
+<?php
+class Change
+{
+  const API_URL = 'https://api.frankfurter.app/latest?from=USD&to=EUR';
+  const FILENAME = 'usd_eur.json';
+
+  public $json = null;
+  private static $_instance = null;
+
+	public static function getInstance($context = null)
+    {
+       	if(is_null(self::$_instance)) {
+       		self::$_instance = new self();
+		}
+		return self::$_instance;
+  }
+
+  public function __construct()
+  {
+    if (!file_exists(self::getFilePath())) {
+      self::storeTaux();
+    }
+    $json = file_get_contents(self::getFilePath());
+    if (!$json) {
+        throw new Exception("Impossible de récupérer les données de change depuis le fichier ".self::getFilePath());
+    }
+    $this->json = json_decode($json, true);
+  }
+
+  public static function getFilePath()
+  {
+    return sfConfig::get('sf_data_dir').'/'.self::FILENAME;
+  }
+
+  public static function storeTaux()
+  {
+    $json = file_get_contents(self::API_URL);
+    if ($json === false) {
+        throw new Exception("Impossible de récupérer les données de change depuis l'API frankfurter");
+    }
+    $save = file_put_contents(self::getFilePath(), $json);
+    if ($save === false) {
+        throw new Exception("Impossible d'écrire les données de change dans le fichier ".self::getFilePath());
+    }
+    return $json;
+  }
+
+  public function getUSDRate()
+  {
+    return round(1 / $this->json["rates"]["EUR"],5);
+  }
+
+  public function getEURRate()
+  {
+    return $this->json["rates"]["EUR"];
+  }
+
+  public function convertToUSD($eur)
+  {
+    return $eur * $this->getUSDRate();
+  }
+
+  public function convertToEUR($usd)
+  {
+    return $usd * $this->getEURRate();
+  }
+
+  public static function getInfos()
+  {
+    if ($json = file_get_contents(self::getFilePath())) {
+      $datas = json_decode($json, true);
+      return (new DateTime($datas['date']))->format('d/m/Y').' : '.$datas['amount'].' '.$datas['base'].' = '.$datas['rates'][array_key_first($datas['rates'])].' '.array_key_first($datas['rates']);
+    }
+    return 'No data to parse';
+  }
+}
