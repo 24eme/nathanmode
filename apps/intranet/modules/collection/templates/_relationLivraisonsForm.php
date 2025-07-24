11495f91aab95bd469c361be494566afefba2935
JB LM <lemetayer.jb@gmail.com>
2025-07-24 17:25:22 +0200

    présentation en tableau des lignes de livraison

--- apps/intranet/modules/collection/templates/_relationLivraisonsForm.php
+++ apps/intranet/modules/collection/templates/_relationLivraisonsForm.php
@@ -1,12 +1,33 @@
-<div id="form_livraisons_container">
-    <?php foreach($form['livraisons'] as $item_form): ?>
-        <?php echo include_partial('collection/relationLivraisonsItem', array('form' => $item_form)); ?>
-    <?php endforeach; ?>
-</div>
-
-<div class="setAdd">
-    <a href="#" class="btAdd lien_ajouter_ligne" data-template="#template_livraisons" data-container="#form_livraisons_container">Ajouter une livraison</a>
-</div>
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+    <tr>
+        <td>
+          <table width="100%" border="0" cellpadding="0" cellspacing="0" class="subTab">
+              <tr>
+                  <th style="width: 12%;"><label>Colori</label></th>
+                  <th style="width: 8%;"><label>Quantité</label></th>
+                  <th style="width: 8%;"><label>Prix de vente</label></th>
+                  <th style="width: 12%;"><label>Escompte</label></th>
+                  <th style="width: 10%;"><label>Date de livraison</label></th>
+                  <th><label>Adresse de livraison</label></th>
+                  <th style="width: 8%;"><label>Num facture</label></th>
+                  <th style="width: 10%;"><label>PDF facture</label></th>
+                  <th style="width: 10%;"><label>Packing list</label></th>
+                  <th style="width: 1%;"><label>&nbsp;</label></th>
+              </tr>
+              <tbody id="form_livraisons_container">
+                  <?php foreach($form['livraisons'] as $item_form): ?>
+                          <?php echo include_partial('collection/relationLivraisonsItem', array('form' => $item_form)); ?>
+                  <?php endforeach; ?>
+              </tbody>
+              <tr>
+                  <td colspan="10" class="px-0">
+                      <a id="ajouter_livraison" class="btPlus right lien_ajouter_ligne" data-template="#template_livraisons" data-container="#form_livraisons_container" href="#">+</a>
+                  </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+  </table>
 
 <script id="template_livraisons" type="text/x-jquery-tmpl">
     <?php echo include_partial('collection/relationLivraisonsItem', array('form' => $form->getTemplate('livraisons'))); ?>
--- apps/intranet/modules/collection/templates/_relationLivraisonsItem.php
+++ apps/intranet/modules/collection/templates/_relationLivraisonsItem.php
@@ -1,104 +1,42 @@
-<div class="relation_item_form">
-    <table width="50%" border="0" cellpadding="0" cellspacing="0" style="margin-top: 1rem; border-bottom: 1px solid #E1E1E1;">
-        <tr>
-            <td width="150"><?php echo $form['colori']->renderLabel() ?>&nbsp;:</td>
-            <td>
-                <?php echo $form['colori']->render() ?>
-                <?php echo $form['colori']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?php echo $form['metrage']->renderLabel() ?>&nbsp;:</td>
-            <td>
-                <?php echo $form['metrage']->render() ?>
-                <?php echo $form['metrage']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?php echo $form['piece_categorie']->renderLabel() ?>&nbsp;:</td>
-            <td>
-                <?php echo $form['piece_categorie']->render(array('class' => 'chosen')) ?>
-                <?php echo $form['piece_categorie']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?php echo $form['piece']->renderLabel() ?>&nbsp;:</td>
-            <td>
-                <?php echo $form['piece']->render() ?>
-                <?php echo $form['piece']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?php echo $form['prix']->renderLabel() ?>&nbsp;:</td>
-            <td>
-                <?php echo $form['prix']->render() ?>
-                <?php echo $form['prix']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?php echo $form['devise_id']->renderLabel() ?>&nbsp;:</td>
-            <td>
-                <?php echo $form['devise_id']->render(array('class' => 'chosen')) ?>
-                <?php echo $form['devise_id']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?php echo $form['escompte']->renderLabel() ?>&nbsp;:</td>
-            <td>
-                <?php echo $form['escompte']->render() ?>
-                <?php echo $form['escompte']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?php echo $form['escompte_devise_id']->renderLabel() ?>&nbsp;:</td>
-            <td>
-                <?php echo $form['escompte_devise_id']->render(array('class' => 'chosen')) ?>
-                <?php echo $form['escompte_devise_id']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?php echo $form['adresse_livraison']->renderLabel() ?>&nbsp;:</td>
-            <td>
-                <?php echo $form['adresse_livraison']->render() ?>
-                <?php echo $form['adresse_livraison']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?php echo $form['date']->renderLabel() ?>&nbsp;:</td>
-            <td style="text-align:left;">
-                <?php echo $form['date']->render() ?>
-                <?php echo $form['date']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?php echo $form['num_facture']->renderLabel() ?>&nbsp;:</td>
-            <td>
-                <?php echo $form['num_facture']->render() ?>
-                <?php echo $form['num_facture']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td style="vertical-align:top;">
-                <?php echo $form['fichier']->renderLabel() ?><img src="/css/img/pdf.gif" width="18" height="19" alt="" align="absbottom" />&nbsp;:
-            </td>
-            <td class="uploadFile">
-                <?php echo $form['fichier']->render(array('class' => 'input')) ?>
-                <?php echo $form['fichier']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-            <td style="vertical-align:top;">
-                <?php echo $form['packing_list']->renderLabel() ?><img src="/css/img/pdf.gif" width="18" height="19" alt="" align="absbottom" />&nbsp;:
-            </td>
-            <td class="uploadFile">
-                <?php echo $form['packing_list']->render(array('class' => 'input')) ?>
-                <?php echo $form['packing_list']->renderError() ?>
-            </td>
-        </tr>
-        <tr>
-          <td colspan="2" align="right" style="padding-right: 8%;">
-            <a class="lien_supprimer_ligne" href="#">supprimer</a>
-          </td>
-        </tr>
-    </table>
-</div>
+<tr class="relation_item_form">
+  <td>
+      <?php echo $form['colori']->render(array('class' => 'colori')) ?>
+      <?php echo $form['colori']->renderError() ?>
+  </td>
+  <td>
+      <?php echo $form['piece']->render(array('class' => 'quantite')) ?>
+      <?php echo $form['piece']->renderError() ?>
+  </td>
+  <td>
+      <?php echo $form['prix']->render(array('class' => 'input-float prix_vente')) ?>
+      <?php echo $form['prix']->renderError() ?>
+  </td>
+  <td style="width: ">
+      <?php echo $form['escompte']->render(array('class' => 'small input-float')) ?>
+      <?php echo $form['escompte_devise_id']->render(array('class' => 'smallchosen')) ?>
+      <?php echo $form['escompte']->renderError() ?>
+  </td>
+  <td style="text-align:left;">
+      <?php echo $form['date']->render() ?>
+      <?php echo $form['date']->renderError() ?>
+  </td>
+  <td>
+      <?php echo $form['adresse_livraison']->render() ?>
+      <?php echo $form['adresse_livraison']->renderError() ?>
+  </td>
+  <td>
+      <?php echo $form['num_facture']->render() ?>
+      <?php echo $form['num_facture']->renderError() ?>
+  </td>
+  <td class="uploadFile">
+      <?php echo $form['fichier']->render(array('class' => 'input')) ?>
+      <?php echo $form['fichier']->renderError() ?>
+  </td>
+  <td class="uploadFile">
+      <?php echo $form['packing_list']->render(array('class' => 'input')) ?>
+      <?php echo $form['packing_list']->renderError() ?>
+  </td>
+  <td class="px-1">
+      <a class="lien_supprimer_ligne" href="#">X</a>
+  </td>
+</tr>
