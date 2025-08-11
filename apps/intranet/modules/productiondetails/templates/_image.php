e9eb5895f3ccc293f719d849001a4f6a15e73aa3
chachathecool <char.var@posteo.com>
2025-08-11 17:41:15 +0200

    Style: centrer les images dans la colonne
    Sur le listing production, on centre
    les images produit dans la colonne

--- apps/intranet/modules/productiondetails/templates/_image.php
+++ apps/intranet/modules/productiondetails/templates/_image.php
@@ -1,3 +1,3 @@
 <?php if ($collection_detail->getImage()) : ?>
-<img height="50" src="<?php echo CollectionDetailTable::getInstance()->getUploadPath(false).$collection_detail->getImage() ?>" />
+<img height="50" style="display:block; margin:auto;" src="<?php echo CollectionDetailTable::getInstance()->getUploadPath(false).$collection_detail->getImage() ?>" />
 <?php endif ?>
