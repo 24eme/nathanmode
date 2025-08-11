1c5d515fa959b5bd5048f6afc904108c6598a09e
chachathecool <char.var@posteo.com>
2025-08-11 17:38:13 +0200

    Feat: retirer la balise img
    Lorqu'il n'y a pas d'image produit,
    la balise img n'est pas affichée

--- apps/intranet/modules/productiondetails/templates/_image.php
+++ apps/intranet/modules/productiondetails/templates/_image.php
@@ -1,1 +1,3 @@
+<?php if ($collection_detail->getImage()) : ?>
 <img height="50" src="<?php echo CollectionDetailTable::getInstance()->getUploadPath(false).$collection_detail->getImage() ?>" />
+<?php endif ?>
