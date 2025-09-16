660a351710c76e7501c5d6d34474bc2f70c548b4
chachathecool <char.var@posteo.com>
2025-09-16 12:21:54 +0200

    Feat: rediriger vers la création d'une nouvelle commande
    Lorsqu'on clique sur le bouton nouveau dans production détail,
    on est redirigé vers une nouvelle commande

---
+++ apps/intranet/modules/productiondetails/templates/_list_actions.php
@@ -0,0 +1,3 @@
+<?php echo link_to(__('Commandes Soldées', array(), 'messages'), 'production/CommandesSoldees', array()) ?>
+<?php echo link_to(__('CSV', array(), 'messages'), 'production/ListCsv', array()) ?>
+<?php echo link_to(__('Nouveau', array()), 'collection_production_new', array()) ?>
