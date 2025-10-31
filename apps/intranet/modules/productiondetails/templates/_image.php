3e081cf636663e38c55807df7265748048157cc7
chachathecool <char.var@posteo.com>
2025-11-01 00:39:09 +0100

    Feat: ouvrir l'image du listing en grand quand on clique dessus

--- apps/intranet/modules/productiondetails/templates/_image.php
+++ apps/intranet/modules/productiondetails/templates/_image.php
@@ -1,3 +1,19 @@
 <?php if ($collection_detail->getImage()) : ?>
-<img height="50" style="display:block; margin:auto;" src="<?php echo CollectionDetailTable::getInstance()->getUploadPath(false).$collection_detail->getImage() ?>" />
+	<?php
+		$url = CollectionDetailTable::getInstance()->getUploadPath(false).$collection_detail->getImage();
+		$width = 600;
+		$height = 400 ;
+	?>
+
+	<a href="javascript: open_popup(<?php echo "'".$url."',".$height.",".$width; ?>)"><img height="50" style="display:block; margin:auto;" src="<?php echo CollectionDetailTable::getInstance()->getUploadPath(false).$collection_detail->getImage() ?>" /></a>
+
+	<script type="text/javascript">
+
+	function open_popup(url,height,width)
+	{
+		popup_top=((screen.height-height)/2);
+		popup_left=((screen.width-width)/2);
+		window.open(url,"image","top="+popup_top+", left="+popup_left+", height="+height+", width="+width);
+	}
+	</script>
 <?php endif ?>
