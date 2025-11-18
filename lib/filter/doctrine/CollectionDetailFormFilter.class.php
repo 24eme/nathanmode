bc404eab7903441965cf806ef546712e643f3ef8
JB LM <lemetayer.jb@gmail.com>
2025-11-18 18:32:04 +0100

    on fait fonctionner les filtres du listing productiondetail

--- lib/filter/doctrine/CollectionDetailFormFilter.class.php
+++ lib/filter/doctrine/CollectionDetailFormFilter.class.php
@@ -38,13 +38,15 @@ class CollectionDetailFormFilter extends BaseCollectionDetailFormFilter
             ));
 
         $this->setWidget('date_commande',
-            new sfWidgetFormFilterDate(
-                array(
-                    'from_date' => new sfWidgetFormDate(array('format' => '%year%-%month%-%day%')),
-                    'to_date' => new sfWidgetFormDate(array('format' => '%year%-%month%-%day%')),
-                    'template' => 'du %from_date%<br />au %to_date%'
-                    )
-            ));
+      		new sfWidgetFormFilterDate(
+      			array(
+      				'from_date' => new sfWidgetFormInput(array('type' => 'date')),
+      				'to_date' => new sfWidgetFormInput(array('type' => 'date')),
+      				'template' => 'du %from_date%<br />au %to_date%'
+      			)
+      		)
+      	);
+
         $this->setValidator('date_commande', new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDate(array('required' => false)))));
         $this->getWidget('date_commande')->setOption('with_empty', false);
 
@@ -55,31 +57,38 @@ class CollectionDetailFormFilter extends BaseCollectionDetailFormFilter
 
 
         $this->setWidget('date_livraison',
-            new sfWidgetFormFilterDate(
-                array(
-                    'from_date' => new sfWidgetFormDate(array('format' => '%year%-%month%-%day%')),
-                    'to_date' => new sfWidgetFormDate(array('format' => '%year%-%month%-%day%')),
-                    'template' => 'du %from_date%<br />au %to_date%'
-                )
-            ));
+          new sfWidgetFormFilterDate(
+            array(
+              'from_date' => new sfWidgetFormInput(array('type' => 'date')),
+              'to_date' => new sfWidgetFormInput(array('type' => 'date')),
+              'template' => 'du %from_date%<br />au %to_date%'
+            )
+          )
+        );
         $this->setValidator('date_livraison', new sfValidatorDateRange(array('required' => false, 'from_date' => new sfValidatorDate(array('required' => false)), 'to_date' => new sfValidatorDateTime(array('required' => false)))));
         $this->getWidget('date_livraison')->setOption('with_empty', false);
 
 
+        $this->setWidget('piece_categorie', new sfWidgetFormChoice(array('choices' => $this->getPieceCategories(), 'multiple' => false)));
+        $this->setValidator('piece_categorie', new sfValidatorPass(array('required' => false)));
+
         $this->setWidget('situation', new sfWidgetFormChoice(array('choices' => $this->getSituations(), 'multiple' => false)));
         $this->setValidator('situation', new sfValidatorPass(array('required' => false)));
 
-        $this->setWidget('qualite', new sfWidgetFormChoice(array('choices' => $this->getQualites(), 'multiple' => false)));
-        $this->setValidator('qualite', new sfValidatorPass(array('required' => false)));
+        /*$this->setWidget('qualite', new sfWidgetFormChoice(array('choices' => $this->getQualites(), 'multiple' => false)));
+        $this->setValidator('qualite', new sfValidatorPass(array('required' => false)));*/
 
         $this->setWidget('nb_relance', new sfWidgetFormFilterInput());
         $this->setValidator('nb_relance', new sfValidatorPass(array('required' => false)));
 
-        $this->getWidget('piece_categorie')->setOption('with_empty', false);
-        $this->getWidget('image')->setOption('with_empty', false);
         $this->getWidget('colori')->setOption('with_empty', false);
         $this->getWidget('piece')->setOption('with_empty', false);
         $this->getWidget('prix')->setOption('with_empty', false);
+        $this->getWidget('nb_relance')->setOption('with_empty', false);
+        $this->getWidget('qualite')->setOption('with_empty', false);
+
+        $this->setWidget('image', new WidgetFormInputDisabled());
+      	$this->setValidator('image', new sfValidatorPass(array('required' => false)));
   }
 
 
@@ -88,36 +97,16 @@ class CollectionDetailFormFilter extends BaseCollectionDetailFormFilter
       return ['' => ''] + array_reverse($saisons, true);
   }
 
-  public function addSaisonIdColumnQuery($query, $field, $values) {
-      $query->addWhere(sprintf('%s.%s = ?', 'c', 'saison_id'), $values);
-  }
-
   public function getFournisseurs() {
       $fournisseurs = FournisseurTable::getInstance()->getListeTriee();
       asort($fournisseurs);
       return ['' => ''] + $fournisseurs;
   }
 
-  public function addFournisseurIdColumnQuery($query, $field, $values) {
-      $query->addWhere(sprintf('%s.%s = ?', 'c', 'fournisseur_id'), $values);
-  }
-
   public function getClients() {
       $clients = ClientTable::getInstance()->getListeTriee();
       asort($clients);
       return ['' => ''] + $clients;
-  }
-
-  public function addClientIdColumnQuery($query, $field, $values) {
-      $query->addWhere(sprintf('%s.%s = ?', 'c', 'client_id'), $values);
-  }
-
-  public function addDateCommandeColumnQuery($query, $field, $values) {
-      $query->addWhere(sprintf('%s.%s BETWEEN ? AND ?', 'c', 'date_commande'), array_values($values));
-  }
-
-  public function addNumCommandeColumnQuery($query, $field, $values) {
-      $query->addWhere(sprintf('%s.%s = ?', 'c', 'num_commande'), array_values($values));
   }
 
   public function getSituations() {
@@ -134,6 +123,95 @@ class CollectionDetailFormFilter extends BaseCollectionDetailFormFilter
           $result[$val['qualite']] = $val['qualite'];
       }
       return $result;
+  }
+
+  public function getPieceCategories() {
+      return ['' => ''] + PieceCategories::getListe();
+  }
+
+  public function addSaisonIdColumnQuery($query, $field, $values)
+  {
+    $value = $this->extractValue($values);
+    if (!$value) return;
+    $query->addWhere(sprintf('%s.%s = ?', 's', 'id'), $value);
+  }
+
+  public function addFournisseurIdColumnQuery($query, $field, $values)
+  {
+    $value = $this->extractValue($values);
+    if (!$value) return;
+    $query->addWhere(sprintf('%s.%s = ?', 'f', 'id'), $value);
+  }
+
+  public function addClientIdColumnQuery($query, $field, $values)
+  {
+    $value = $this->extractValue($values);
+    if (!$value) return;
+    $query->addWhere(sprintf('%s.%s = ?', 'cl', 'id'), $value);
+  }
+
+  public function addSituationColumnQuery($query, $field, $values)
+  {
+    $value = $this->extractValue($values);
+    if (!$value) return;
+    $query->addWhere(sprintf('%s = ?', 'situation'), $value);
+  }
+
+  public function addPieceCategorieColumnQuery($query, $field, $values)
+  {
+    $value = $this->extractValue($values);
+    if (!$value) return;
+    $query->addWhere(sprintf('%s = ?', 'piece_categorie'), $value);
+  }
+
+  public function addQualiteColumnQuery($query, $field, $values)
+  {
+    $value = $this->extractValue($values);
+    if (!$value) return;
+    $query->addWhere(sprintf('%s = ?', 'qualite'), $value);
+  }
+
+  public function addNumCommandeColumnQuery($query, $field, $values)
+  {
+    $value = $this->extractValue($values);
+    if (!$value) return;
+    $query->addWhere(sprintf('%s = ?', 'num_commande'), $value);
+  }
+
+  public function addColoriColumnQuery($query, $field, $values)
+  {
+    $value = $this->extractValue($values);
+    if (!$value) return;
+    $query->addWhere("colori LIKE '%".$value."%'");
+  }
+
+  public function addPieceColumnQuery($query, $field, $values)
+  {
+    $value = $this->extractValue($values);
+    if (!$value) return;
+    $query->addWhere('(piece = '.$value.' OR metrage = '.$value.')');
+  }
+
+  public function addNbRelanceColumnQuery($query, $field, $values)
+  {
+    $value = $this->extractValue($values);
+    if (!$value) return;
+    $query->addWhere(sprintf('%s = ?', 'nb_relance'), $value);
+  }
+
+  public function addDateCommandeColumnQuery($query, $field, $values)
+  {
+    $query->addWhere(sprintf('%s BETWEEN ? AND ?', 'date_commande'), array_values($values));
+  }
+
+  public function addDateLivraisonColumnQuery($query, $field, $values)
+  {
+    $query->addWhere(sprintf('%s BETWEEN ? AND ?', 'date_livraison'), array_values($values));
+  }
+
+  private function extractValue($values)
+  {
+    return (is_array($values))? array_values($values)[0] : $values;
   }
 
 }
