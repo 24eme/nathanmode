0c1f0adf388c09fb6587697c6e0664784195b634
Vincent LAURENT <vince.laurent@gmail.com>
2025-12-02 10:16:56 +0100

    id dans les exports

--- apps/intranet/modules/productiondetails/lib/productiondetailsGeneratorConfiguration.class.php
+++ apps/intranet/modules/productiondetails/lib/productiondetailsGeneratorConfiguration.class.php
@@ -11,6 +11,6 @@
 class productiondetailsGeneratorConfiguration extends BaseProductiondetailsGeneratorConfiguration
 {
     public function getListExport() {
-        return ["saison", "fournisseur", "client", "date_commande", "num_commande", "situation", "categorie", "qualite", "colori", "quantite", "prix", "Devise", "nbrelance", "date_livraison_demandee", "date_livraison_prevue", "paiement"];
+        return ["saison", "fournisseur", "client", "date_commande", "num_commande", "situation", "categorie", "qualite", "colori", "quantite", "prix", "Devise", "nbrelance", "date_livraison_demandee", "date_livraison_prevue", "paiement", "id", "collection_id"];
     }
 }
