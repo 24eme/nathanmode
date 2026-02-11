bbd1320f0edf2ca8d931862f1ba9fb42f1c2fc96
chachathecool <char.var@posteo.com>
2026-02-11 16:11:41 +0100

    Refacto: retirer console log

--- apps/intranet/modules/productiondetails/templates/_list_actions.php
+++ apps/intranet/modules/productiondetails/templates/_list_actions.php
@@ -1,12 +1,12 @@
+<span class="action-buttons">
 <?php echo link_to(__('Commandes Soldées', array(), 'messages'), 'productiondetails/CommandesSoldees', array()) ?>
 <?php echo link_to(__('CSV', array(), 'messages'), 'productiondetails/ListCsv', array()) ?>
-<?php echo link_to(__('Nouveau', array()), 'collection_production_new', array()) ?>
-
+<?php echo link_to(__('Nouveau', array()), 'collection_production_new', array(), array()) ?>
+</span>
 
 <script>
 document.addEventListener("DOMContentLoaded", (event) => {
   document.querySelectorAll('.sf_admin_list_td_commande_soldee img').forEach(function(item) {
-  console.log(item)
     item.closest('tr').style.opacity = '0.5';
   });
 })
