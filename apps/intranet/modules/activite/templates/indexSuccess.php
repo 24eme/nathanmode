f0d1b7c683392c19287c3cb15e4aedd2ca4a145a
Vincent LAURENT <vince.laurent@gmail.com>
2026-07-20 00:12:50 +0200

    Ajustement du css

--- apps/intranet/modules/activite/templates/indexSuccess.php
+++ apps/intranet/modules/activite/templates/indexSuccess.php
@@ -1,6 +1,6 @@
 <div class="text-end">
 <a class="btn btn-light btn-sm float-end mb-2" style="text-decoration: none;" href="<?php echo url_for('activiteRapport') ?>"><i class="bi bi-clipboard-data"></i> Rapport global</a>
-<a class="btn btn-light btn-sm float-end mb-2 ms-1" style="text-decoration: none;" href="<?php echo url_for('activiteGraph') ?>"><i class="bi bi-graph-up"></i> Graphique du CA</a>
+<a class="btn btn-light btn-sm float-end mb-2 me-1" style="text-decoration: none;" href="<?php echo url_for('activiteGraph') ?>"><i class="bi bi-graph-up"></i> Graphique du CA</a>
 </div>
 
 <?php if ($dashboard = sfConfig::get('app_commercial_activity_metabase_dashboard')): ?>
